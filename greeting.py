f8370e7268d4ef433fa97e5d00dec581db3248f8
Edrine Spencer <edrinespencer5824@icloud.com>
2026-09-01 14:48:29 +0100

    Add initial Python greeting program

---
+++ greeting.py
@@ -0,0 +1,7 @@
+# Unit 2 GitHub for Python - Version Control Practice
+# This first version displays a simple greeting.
+
+name = "Edrine"
+
+print("Hello " + name)
+print("Welcome to my GitHub Python project.")
